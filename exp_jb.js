2880fc504e7353db648cc7d3aa6141d315a640a0
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2021-07-04 15:08:36 -0300

    Add files via upload

--- exp_jb.js
+++ exp_jb.js
@@ -150687,7 +150687,7 @@ var _ = malloc_nogc.pop();
 if (main_ret == 179 || main_ret == 0) {
 	localStorage.passcount = ++localStorage.passcount;window.passCounter.innerHTML=localStorage.passcount;
 	localStorage.infoDesb = "<font style='color:#6089f6'>Exploração do kernel realizada.</font>";window.infoDesb.innerHTML=localStorage.infoDesb; 
-	load_hen();
+	let pausa_hen = setTimeout(load_hen, 2000);
 } 
 else {
 	localStorage.failcount = ++localStorage.failcount;window.failCounter.innerHTML=localStorage.failcount;
